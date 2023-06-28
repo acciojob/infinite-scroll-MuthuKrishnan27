4c71c04e47aa7638ee6478976965db852b5df008
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-28 13:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-//your code here!
+// Function to add list items
+var list = document.getElementById("infi-list");
+function addListItems() {
+  for (var i = 0; i < 2; i++) {
+    var listItem = document.createElement("li");
+    listItem.textContent = "List Item " + (list.children.length + 1);
+    list.appendChild(listItem);
+  }
+}
 
+// Function to check if user has reached the end of list
+function checkEndOfList() {
+  var list = document.getElementById("infi-list");
+  var lastListItem = list.lastElementChild;
+  var lastListItemRect = lastListItem.getBoundingClientRect();
+
+  if (lastListItemRect.bottom <= list.getBoundingClientRect().bottom) {
+    addListItems();
+  }
+}
+
+// Add event listener for scroll event on list element
+var list = document.getElementById("infi-list");
+list.addEventListener("scroll", checkEndOfList);
+
+// Add initial list items
+initialItems();
+function initialItems(){
+	for(var i=0;i<8;i++){
+		var listItem = document.createElement("li");
+		listItem.textContent = "List Item " + (list.children.length + 1);
+		list.appendChild(listItem);
+	}
+}
